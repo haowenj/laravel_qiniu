6b686b21b011ef6ce6234cc1a8a92919d6927839
haowenju <haowenju@myxiaoban.com>
2018-06-06 16:15:09 +0800

    回调写入数据库

--- app/Http/Controllers/QiuniuController.php
+++ app/Http/Controllers/QiuniuController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
@@ -14,17 +15,18 @@ class QiuniuController extends Controller
      * 获取上传token
      * @return \Illuminate\Http\JsonResponse
      */
-    public function getToken()
+    private function getToken($key)
     {
         $accessKey = env('QINIU_ACCESSKEY');
         $secretKey = env('QINIU_SECRETKEY');
         $bucket = env('QINIU_BUCKET');
         $policy = array(
             'callbackUrl' => env('APP_URL') . '/qiniuCallback',
-            'callbackBody' => 'fkey=$(key)'
+            'callbackBody' => 'fkey=$(key)',
+            'saveKey' => $key
         );
         $auth = new Auth($accessKey, $secretKey);
-        $token = $auth->uploadToken($bucket, '', '', $policy);
+        $token = $auth->uploadToken($bucket, null, '', $policy);
         return $token;
     }
 
@@ -42,7 +44,7 @@ class QiuniuController extends Controller
             $ext = $file->getClientOriginalExtension();     // 扩展名
             $realPath = $file->getRealPath();   //临时文件的绝对路径
             $type = $file->getClientMimeType();     // image/jpeg
-            $filename = date('Y-m-d-H-i-s') . '-' . uniqid() . '.' . $ext;
+            $filename = date('YmdHis') . '' . uniqid() . '.' . $ext;
 
             // 要上传文件的本地路径
             $filePath =  $realPath;
@@ -50,9 +52,9 @@ class QiuniuController extends Controller
             $key = $filename;
 
             //拿到上传token
-            $token = $this->getToken();
+            $token = $this->getToken($key);
             $uploadMgr = new UploadManager();
-            list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
+            list($ret, $err) = $uploadMgr->putFile($token, null, $filePath);
 
             if ($err !== null) {
                 return $err;
@@ -64,6 +66,9 @@ class QiuniuController extends Controller
 
     public function qiniuCallback(Request $request)
     {
-        Log::info(json_encode($request->all()));
+        $filename = $request->input('fkey');
+        Image::create(compact('filename'));
+        $url = env('QINIU_IMGURL') . $filename;
+        return $url;
     }
 }
